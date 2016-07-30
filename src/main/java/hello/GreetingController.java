38ae0489a37015aa5ff72f875221a68fa135ba12
Rimvydas Kulda <rimvydas.kulda@gmail.com>
2016-07-30 12:32:42 +0300

    removed redundant code, proof that openCV lib actually works

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,23 +1,19 @@
 package hello;
 
-import java.util.concurrent.atomic.AtomicLong;
+import org.opencv.core.Core;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
     static {
-        nu.pattern.OpenCV.loadShared();
+        //nu.pattern.OpenCV.loadShared();
         System.loadLibrary(org.opencv.core.Core.NATIVE_LIBRARY_NAME);
     }
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    public String greeting() {
+        return "Welcome to OpenCV " + Core.VERSION;
     }
 }
