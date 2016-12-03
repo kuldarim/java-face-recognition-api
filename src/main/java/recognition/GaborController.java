f6e1c2f5f5acb0600457ba83de1f32829007d911
Rimvydas Kulda <rimvydas.kulda@gmail.com>
2016-12-03 13:12:11 +0200

    extended calculation

--- src/main/java/recognition/GaborController.java
+++ src/main/java/recognition/GaborController.java
@@ -48,8 +48,32 @@ public class GaborController {
     return matService.matToJSON(cropped);
   }
 
-  @RequestMapping("/create/variance/mats")
-  public String createVarianceMats() {
+  @RequestMapping("/create/self/mean/mats")
+   public String createSelfMeanMats() {
+
+    ArrayList<Double[][]> varianceMatsP1 = this.readMatsByPerson("p1_");
+    System.out.println("varianceMatsP1 done");
+    Double[][] meanP1 = gaborService.calculateMeanForVarianceMats(varianceMatsP1);
+    System.out.println("meanP1 done");
+    this.storeMatInFile("/mean/self/p1.txt", meanP1);
+
+    ArrayList<Double[][]> varianceMatsP2 = this.readMatsByPerson("p2_");
+    System.out.println("varianceMatsP2 done");
+    Double[][] meanP2 = gaborService.calculateMeanForVarianceMats(varianceMatsP2);
+    System.out.println("meanP2 done");
+    this.storeMatInFile("/mean/self/p2.txt", meanP2);
+
+    ArrayList<Double[][]> varianceMatsP3 = this.readMatsByPerson("p3_");
+    System.out.println("varianceMatsP3 done");
+    Double[][] meanP3 = gaborService.calculateMeanForVarianceMats(varianceMatsP3);
+    System.out.println("meanP3 done");
+    this.storeMatInFile("/mean/self/p3.txt", meanP3);
+
+    return "yey";
+  }
+
+  @RequestMapping("/create/others/mean/mats")
+  public String createOthersMeanMats() {
     ArrayList<Mat> originalImagesP1 = readImages("p1/resized/p1_", 6);
     ArrayList<Mat> originalImagesP2 = readImages("p2/resized/p2_", 6);
     ArrayList<Mat> originalImagesP3 = readImages("p3/resized/p3_", 6);
@@ -75,6 +99,27 @@ public class GaborController {
     return "yey";
   }
 
+  @RequestMapping("/create/variance/mats")
+  public String createVarianceMats() {
+    ArrayList<Mat> originalImagesP1 = readImages("p1/resized/p1_", 6);
+    ArrayList<Mat> originalImagesP2 = readImages("p2/resized/p2_", 6);
+    ArrayList<Mat> originalImagesP3 = readImages("p3/resized/p3_", 6);
+
+    System.out.println("varianceMatsP1 started");
+    this.storeVarianceMatsForImages(originalImagesP1, "variance/p1_");
+    System.out.println("varianceMatsP1 done");
+
+    System.out.println("varianceMatsP2 started");
+    this.storeVarianceMatsForImages(originalImagesP2, "variance/p2_");
+    System.out.println("varianceMatsP2 done");
+
+    System.out.println("varianceMatsP3 started");
+    this.storeVarianceMatsForImages(originalImagesP3, "variance/p3_");
+    System.out.println("varianceMatsP3 done");
+
+    return "yey";
+  }
+
   @RequestMapping("/write")
   public String write() {
     Double[][] test = new Double[1][1];
@@ -87,6 +132,14 @@ public class GaborController {
   public String read() {
     Double[][] mat = this.readMatFromFile("test");
     return String.valueOf(mat[0][0]);
+  }
+  private void storeVarianceMatsForImages(ArrayList<Mat> images, String fileName) {
+    int i = 1;
+    for (Mat original: images) {
+      Double[][] variance = gaborService.createGaborVarianceMatForImage(original);
+      this.storeMatInFile(fileName + i +".txt", variance);
+      i++;
+    }
   }
 
   private ArrayList<Double[][]> getVarianceMatsForImages(ArrayList<Mat> images) {
@@ -125,8 +178,19 @@ public class GaborController {
     }
   }
 
+  private ArrayList<Double[][]> readMatsByPerson(String person) {
+    ArrayList<Double[][]> varianceMats = new ArrayList<>();
+
+    for (int i = 1; i<=6; i++) {
+      Double[][] mat = this.readMatFromFile("variance/" + person + i + ".txt");
+      varianceMats.add(mat);
+    }
+
+    return varianceMats;
+  }
+
   private Double[][] readMatFromFile(String fileName) {
-    try(FileInputStream in = new FileInputStream("src/main/resources/test.txt");
+    try(FileInputStream in = new FileInputStream("src/main/resources/calculated/" + fileName);
         ObjectInputStream s = new ObjectInputStream(in)) {
       Double[][] mat = (Double[][])s.readObject();
       return mat;
